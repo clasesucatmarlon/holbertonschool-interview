0b748476db88c1f1ac7be339e4e836017734d466
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-01-27 11:40:05 -0500

    Lockboxes V7.0

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,12 +4,13 @@ You have n number of locked boxes in front of you. Each box is numbered
 sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
 """
 
+
 def canUnlockAll(boxes):
     """ Write a method that determines if all the boxes can be opened
     """
     array = [0]
     if (boxes == []):
-        return True
+        return False
     for key in array:
         for key_box in boxes[key]:
             if key_box not in array:
