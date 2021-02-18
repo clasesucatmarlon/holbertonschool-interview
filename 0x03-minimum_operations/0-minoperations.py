eefb8b6f65a2d7c704748149017c84df38bdac36
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-02-17 20:42:27 -0500

    Minimum Operations

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
+"""
+Given a number n, write a method that calculates the
+fewest number of operations needed to result in
+exactly n H characters in the file.
+"""
+
 import math
 
 
-def minOperations(n):
-    """ 
-    Calculates the fewest number of operations needed to result in
-    exactly n H characters.
-    Return: the fewest number of operations needed to result in n H characters
-    """
-    add = 0
-    if n <= 1:
-        return add
-
-    limit = int(math.sqrt(n) + 1)
-    for i in range(2, limit):
+def factors(n):
+    """calculates the factors of n number"""
+    mylist = []
+    while n % 2 == 0:
+        mylist.append(2)
+        n = n / 2
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
         while n % i == 0:
-            add += i
-            n = n // i
-    if n > 1:
-        add += n
-    return add
+            mylist.append(i)
+            n = n / i
+    if n > 2:
+        mylist.append(n)
+    return mylist
+
+
+def minOperations(n):
+    """calculate the minimum operations"""
+    if type(n) != int or n < 2:
+        return 0
+    else:
+        numOperations = sum(factors(n))
+        return int(numOperations)
