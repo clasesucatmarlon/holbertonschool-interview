6dc85600b558d9ecb3ce251ad521072a33a9da7f
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-02-25 11:12:24 -0500

    Sandpiles sum

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -1,120 +1,106 @@
-#include <stdio.h>
+#include "sandpiles.h"
 
 /**
- * grid_sum - sum 2 grids
- * @grid1: first matrix
- * @grid2: second matrix
+ * sum_two_grid - sum of two sandpiles
+ * @grid1: sandpiles stable
+ * @grid2: sandpiles stable
+ * Return: void function
  */
-
-void grid_sum(int grid1[3][3], int grid2[3][3])
+static void sum_two_grid(int grid1[3][3], int grid2[3][3])
 {
-    int i = 0;
-    int j = 0;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            grid1[i][j] = grid1[i][j] + grid2[i][j];
-        }
-    }
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			grid1[i][j] = grid1[i][j] + grid2[i][j];
 }
 
 /**
- * grid_print - print a grid
- * @grid: matrix
+ * print_grid - print a sandpiles
+ * @grid: sandpiles stable
+ * Return: void function
  */
-
-void grid_print(int grid[3][3])
+static void print_grid(int grid[3][3])
 {
-    int i = 0;
-    int j = 0;
+	int i, j;
 
-    printf("=\n");
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
+	printf("=\n");
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
 }
 
 /**
- * grid_is_ok - check if a grid is ok
- * @grid: matrix
- * Return: 1 if is ok or 0 if not
+ * check_sandpiles - checking if is a sandpiles
+ * @grid: sandpiles stable
+ * Return: int
  */
-
-int grid_is_ok(int grid[3][3])
+static int check_sandpiles(int grid[3][3])
 {
-    int i = 0;
-    int j = 0;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (grid[i][j] > 3)
-                return (0);
-        }
-    }
-    return (1);
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			if (grid[i][j] > 3)
+				return (0);
+	return (1);
 }
 
 /**
- * grid_change - change a grid value
- * @grid1: matrix
+ * converting_into_sandpiles - convert the grid
+ * @grid1: sandpiles stable
+ * Return: void function
  */
-
-void grid_change(int grid1[3][3])
+static void converting_into_sandpiles(int grid1[3][3])
 {
-    int i = 0;
-    int j = 0;
-    int gridx[3][3];
+	int i, j;
+	int grid_count[3][3];
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-            gridx[i][j] = 0;
-    }
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			grid_count[i][j] = 0;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (grid1[i][j] > 3)
-            {
-                grid1[i][j] = grid1[i][j] - 4;
-                if ((i - 1) >= 0 && (i - 1) < 3)
-                    gridx[i - 1][j] += 1;
-                if ((j - 1) >= 0 && (j - 1) < 3)
-                    gridx[i][j - 1] += 1;
-                if ((i + 1) >= 0 && (i + 1) < 3)
-                    gridx[i + 1][j] += 1;
-                if ((j + 1) >= 0 && (j + 1) < 3)
-                    gridx[i][j + 1] += 1;
-            }
-        }
-    }
-    grid_sum(grid1, gridx);
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid1[i][j] > 3)
+			{
+				grid1[i][j] = grid1[i][j] - 4;
+				if ((i - 1 >= 0) && (i - 1 < 3))
+					grid_count[i - 1][j] += 1;
+				if ((j - 1 >= 0) && (j - 1 < 3))
+					grid_count[i][j - 1] += 1;
+				if ((i + 1 >= 0) && (i + 1 < 3))
+					grid_count[i + 1][j] += 1;
+				if ((j + 1 >= 0) && (j + 1 < 3))
+					grid_count[i][j + 1] += 1;
+			}
+		}
+	}
+	sum_two_grid(grid1, grid_count);
 }
 
 /**
- * sandpiles_sum - sum 2 sandpiles
- * @grid1: first matrix
- * @grid2: second matrix
+ * sandpiles_sum - computes the sum of two sandpiles
+ * @grid1: sandpiles stable
+ * @grid2: sandpiles stable
+ * Return: void function
  */
-
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    grid_sum(grid1, grid2);
-    while (!grid_is_ok(grid1))
-    {
-        grid_print(grid1);
-        grid_change(grid1);
-    }
+	sum_two_grid(grid1, grid2);
+
+	while (!check_sandpiles(grid1))
+	{
+		print_grid(grid1);
+		converting_into_sandpiles(grid1);
+	};
 }
