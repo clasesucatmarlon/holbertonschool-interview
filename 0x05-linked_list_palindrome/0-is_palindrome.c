1f2bf047590bb0def848242570179168cef826a9
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-03-04 21:18:22 -0500

    Is Polindrome

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,47 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
-
 /**
- * check_palindrome - a function to check first and last values
- * of a given linked list equal or not
- * @first: double pointer to first node
- * @last: double pointer to last node
- *
- * Return: return true if left node equal right node, otherwise false
+ * len_list - returns the number of elements in list.
+ * @head: pointer to head of linked list
+ * Return: length of single linked list (i)
  */
-
-bool check_palindrome(listint_t **first, listint_t *last)
+int len_list(listint_t **head)
 {
-	if (!last)
-		return (1);
+	int i = 0;
+	listint_t *aux;
 
-	bool result = check_palindrome(first, last->next);
-
-	if (!result)
-		return (0);
-
-	bool result1 = (last->n == (*first)->n);
-	*first = (*first)->next;
-
-	return (result1);
+	aux = *head;
+	while (aux)
+	{
+		aux = aux->next;
+		i++;
+	}
+	return (i);
 }
 
+/**
+ * list_reverse - Reverse the list
+ * @head: pointer to head of linked list
+ * Return: nothing
+ */
+void list_reverse(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *tmp = *head;
+	listint_t *aux = NULL;
+
+	while (tmp != NULL)
+	{
+		aux = tmp->next;
+		tmp->next = prev;
+		prev = tmp;
+		tmp = aux;
+	}
+	*head = prev;
+}
 
 /**
- * is_palindrome - a function to hecks if a singly linked list is a palindrome.
- * @head: double pointer to head node
- *
- * Return: 1 if it is a palindrome, otherwise 0
+ * is_palindrome - checks if the linked list is a palindrome
+ * @head: pointer to head of linked list
+ * Return: 1 if list is palindrome or 0 if it is not palindrome
  */
-
 int is_palindrome(listint_t **head)
 {
+	listint_t *new_head = *head;
+	listint_t *tmp;
+	listint_t *aux = *head;
+	int length_list, middle, x = 0, num = 1;
 
-	if (!(*head) || !head)
+	if (head == NULL)
 		return (1);
-
-	if (check_palindrome(&(*head), *head))
-		return (1);
-	else
-		return (0);
+	length_list = len_list(head);
+	middle = length_list / 2;
+	while (x < middle)
+	{
+		new_head = new_head->next;
+		x++;
+	}
+	if (length_list % 2 != 0)
+		new_head = new_head->next;
+	list_reverse(&new_head);
+	tmp = new_head;
+	aux = *head;
+	while (tmp != NULL)
+	{
+		if (tmp->n != aux->n)
+			num = 0;
+		tmp = tmp->next;
+		aux = aux->next;
+	}
+	return (num);
 }
