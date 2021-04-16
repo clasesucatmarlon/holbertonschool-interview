38fb8e80c9c19e0615b8362d099ec2be60007062
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-04-15 20:32:50 -0500

    Menger sponge file

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "menger.h"
+
+/**
+ * menger - draws a 2D Menger Sponge
+ * @level: level of the menger sponge to draw
+ *
+ */
+void menger(int level)
+{
+	int i, j, result, x, y;
+	char simbol;
+
+	result = pow(3, level);
+
+	for (i = 0; i < result; i++)
+	{
+		for (j = 0; j < result; j++)
+		{
+			simbol = '#';
+			x = i;
+			y = j;
+			while (x > 0)
+			{
+				if (x % 3 == 1 && y % 3 == 1)
+					simbol = ' ';
+				x /= 3;
+				y /= 3;
+			}
+			printf("%c", simbol);
+		}
+		printf("\n");
+	}
+}
