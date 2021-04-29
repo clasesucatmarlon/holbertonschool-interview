ad4094f20c791e35eced310e5b8dab9d19924bd6
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-04-29 14:03:06 -0500

    n Quuens

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-N queens algorithm
-"""
-
+''' N queens algorithm '''
 import sys
 
 if len(sys.argv) != 2:
@@ -10,47 +7,44 @@ if len(sys.argv) != 2:
     exit(1)
 
 try:
-    nQueen = int(sys.argv[1])
+    n_q = int(sys.argv[1])
 except ValueError:
     print("N must be a number")
     exit(1)
 
-if nQueen < 4:
+if n_q < 4:
     print("N must be at least 4")
     exit(1)
 
 
 def solve_nqueens(n):
-    """ self descriptive
-    """
+    '''self descriptive'''
     if n == 0:
         return [[]]
     inner_solution = solve_nqueens(n - 1)
     return [solution + [(n, i + 1)]
-            for i in range(nQueen)
+            for i in range(n_q)
             for solution in inner_solution
             if safe_queen((n, i + 1), solution)]
 
 
 def attack_queen(square, queen):
-    """ self descriptive
-    """
+    '''self descriptive'''
     (row1, col1) = square
     (row2, col2) = queen
-    return (row1 == row2) or (col1 == col2) or \
+    return (row1 == row2) or (col1 == col2) or\
         abs(row1 - row2) == abs(col1 - col2)
 
 
 def safe_queen(sqr, queens):
-    """ self descriptive
-    """
+    '''self descriptive'''
     for queen in queens:
         if attack_queen(sqr, queen):
             return False
     return True
 
 
-for answer in reversed(solve_nqueens(nQueen)):
+for answer in reversed(solve_nqueens(n_q)):
     result = []
     for p in [list(p) for p in answer]:
         result.append([i - 1 for i in p])
